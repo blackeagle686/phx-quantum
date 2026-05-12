9714d9b9221a15bec88c873e1bd0557e4c332f00
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-12 16:42:34 +0300

    Regenerate build files with CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/The_Last_King/OneDrive/Documents/Projects/phx-quantum
+# Build directory: C:/Users/The_Last_King/OneDrive/Documents/Projects/phx-quantum/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("math")
+subdirs("quantum")
+subdirs("tensor")
+subdirs("runtime")
+subdirs("examples")
+subdirs("tests")
